e6de4db34a0944e70c04b1dd0237e631834e7f8d
Peter Kostov <pekostov@gmail.com>
2015-12-16 08:52:08 +0200

    Add grid initialization

--- f#/GameOfLife/GameOfLife.Core/Initialization.fs
+++ f#/GameOfLife/GameOfLife.Core/Initialization.fs
@@ -4,20 +4,26 @@ open System
 
 open Types
 
-let initialize size =
-    if size <= 0 then raise (Exception())
+let initialize side =
+    if side <= 0 then raise (Exception())
     let cells = seq {
-        for x in 0 .. size - 1 do
-            for y in 0 .. size - 1 do
-                yield { X = x; Y = y; IsAlive = true } }
+        for x in 0 .. side - 1 do
+            for y in 0 .. side - 1 do
+                yield { X = x; Y = y; IsAlive = false } }
     { Cells = cells }
 
 let seed grid percent =
     let count = grid.Cells |> Seq.length
     let take = (percent * count) / 100
-    let rnd = Random(0)
-    seq {
-        let i = 
-    }
-
+    let rnd = Random()
+    let shuffleR (r : Random) elements = elements |> Seq.sortBy (fun _ -> r.Next())
+    let aliveCells = shuffleR rnd grid.Cells |> Seq.take take |> List.ofSeq
+    let cells =
+        grid.Cells
+        |> Seq.map
+            (fun x ->
+                match aliveCells |> Seq.contains x with
+                | true -> { x with IsAlive = true }
+                | false -> x)
+    { Cells = cells }
 
--- f#/GameOfLife/GameOfLife.Tests/InitializationTests.fs
+++ f#/GameOfLife/GameOfLife.Tests/InitializationTests.fs
@@ -6,33 +6,37 @@ open FsUnit.Xunit
 open GameOfLife.Core.Types
 open GameOfLife.Core.Initialization
 
+let aliveCells grid =
+    let cells = grid.Cells |> Array.ofSeq
+    cells
+    |> Seq.filter (fun x -> x.IsAlive)
+    |> Seq.length
+
 [<Trait("Category", "Unit")>]
 module ``When initializing grid`` =
-    module ``Given size 100`` =
-        let aliveCells grid =
-            grid.Cells
-            |> Seq.filter (fun x -> not x.IsAlive)
-            |> Seq.length
-
+    module ``Given grid side 10`` =
+        let grid = initialize 10
         [<Fact>]
-        let ``Should initialize it successfully`` () = 
-            let grid = initialize 100
+        let ``Should initialize it successfully`` () =
+            grid.Cells |> Seq.length |> should equal 100
+        [<Fact>]
+        let ``Should have only dead cells`` () = 
             grid |> aliveCells |> should equal 0
 
-    module ``Given size of 0`` = 
+    module ``Given grid side of 0`` = 
         [<Fact>]
         let ``Should fail to initialize the grid`` () = 
             shouldFail (fun () -> initialize 0 |> ignore)
 
-    module ``Given negative size`` =
+    module ``Given negative grid size`` =
         [<Fact>]
         let ``Should fail to initialize the grid`` () = 
             shouldFail (fun () -> initialize -10 |> ignore)
 
 [<Trait("Category", "Unit")>]
 module ``When seeding grid`` =
-    let grid = initialize 100
     module ``Given 10 percent seed of 100 cells`` =
         [<Fact>]
-        let ``Should have 10 cells alive`` ()
+        let ``Should have 10 cells alive`` () =
+            let grid = initialize 10
             seed grid 10 |> aliveCells |> should equal 10
