207601a53eb7b93cda494ad7881e055ad146e696
Peter Kostov <pekostov@gmail.com>
2016-01-03 18:54:50 +0200

    Finish grid seeding

--- f#/GameOfLife/GameOfLife.Tests/InitializationTests.fs
+++ f#/GameOfLife/GameOfLife.Tests/InitializationTests.fs
@@ -20,17 +20,17 @@ module ``When initializing grid`` =
         let ``Should initialize it successfully`` () =
             grid.Cells |> Seq.length |> should equal 100
         [<Fact>]
-        let ``Should have only dead cells`` () = 
+        let ``Should have only dead cells`` () =
             grid |> aliveCells |> should equal 0
 
-    module ``Given grid side of 0`` = 
+    module ``Given grid side of 0`` =
         [<Fact>]
-        let ``Should fail to initialize the grid`` () = 
+        let ``Should fail to initialize the grid`` () =
             shouldFail (fun () -> initialize 0 |> ignore)
 
     module ``Given negative grid size`` =
         [<Fact>]
-        let ``Should fail to initialize the grid`` () = 
+        let ``Should fail to initialize the grid`` () =
             shouldFail (fun () -> initialize -10 |> ignore)
 
 [<Trait("Category", "Unit")>]
@@ -40,3 +40,15 @@ module ``When seeding grid`` =
         let ``Should have 10 cells alive`` () =
             let grid = initialize 10
             seed grid 10 |> aliveCells |> should equal 10
+
+    module ``Given 0 percent seed of 100 cells`` =
+        [<Fact>]
+        let ``Should have 0 cells alive`` () =
+            let grid = initialize 10
+            seed grid 0 |> aliveCells |> should equal 0
+
+    module ``Given 100 percent seed of 100 cells`` =
+        [<Fact>]
+        let ``Should have 100 cells alive`` () =
+            let grid = initialize 10
+            seed grid 100 |> aliveCells |> should equal 100
